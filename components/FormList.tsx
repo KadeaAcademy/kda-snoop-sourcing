37ff53a0e0fa248d579bc29d809e8ab9639c0dce
sambiss1 <sambisselele@gmail.com>
2023-08-23 12:45:51 +0100

    changes : set params blank on reset button

--- components/FormList.tsx
+++ components/FormList.tsx
@@ -130,6 +130,7 @@ export default function FormList() {
       setFilteredData(forms);
       setSelectedFormation("");
       setSelectedLocation("");
+      router.push(`/sourcings?training=&city=`);
       return;
     }
   };
