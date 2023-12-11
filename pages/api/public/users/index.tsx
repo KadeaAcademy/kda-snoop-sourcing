9c893df8c9a56cada16822b51d105d844cae23ec
sambiss1 <samyombobiss@gmail.com>
2023-12-11 15:33:29 +0100

    Feat : create user acount by url

--- pages/api/public/users/index.tsx
+++ pages/api/public/users/index.tsx
@@ -1,16 +1,15 @@
-import type {NextApiRequest, NextApiResponse} from "next";
-import {getSession} from "next-auth/react";
-import {prisma} from "../../../../lib/prisma";
-import {UserRole} from "@prisma/client";
-import {sendVerificationEmail} from "../../../../lib/email";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { prisma } from "../../../../lib/prisma";
+import { UserRole, Form } from "@prisma/client";
+import { sendVerificationEmail } from "../../../../lib/email";
 import getConfig from "next/config";
-import {capturePosthogEvent} from "../../../../lib/posthog";
+import { capturePosthogEvent } from "../../../../lib/posthog";
 import { hashPassword } from "../../../../lib/auth";
-// import { sendPasswordResetNotifyEmail } from "../../../../lib/email";
-import { updateCandidateSourcingOnAirtable } from "../../../../lib/utils";
-import { createToken } from "../../../../lib/jwt";
 
-const {publicRuntimeConfig} = getConfig();
+
+
+const { publicRuntimeConfig } = getConfig();
 
 export default async function handle(
   req: NextApiRequest,
@@ -21,17 +20,27 @@ export default async function handle(
   // Required fields in body: email, password (hashed)
   // Optional fields in body: firstname, lastname
   if (req.method === "POST") {
-    let {user,callbackUrl} = req.body;
-    let {email, trainingSession} = req.query;
-    
+    let { user, callbackUrl } = req.body;
+    let { email, trainingSession } = req.query;
+
     let password: string = "Kadea123";
 
     const hashedPassword = await hashPassword(password);
 
+    user = { ...user, ...{ email: email, password: hashedPassword } };
 
-    user = {...user, ...{email: email, password: hashedPassword}};
+    // const { emailVerificationDisabled } = publicRuntimeConfig;
 
-    const {emailVerificationDisabled} = publicRuntimeConfig;
+    const form = await prisma.form.findFirst({
+      where: {
+        airtableTrainingSessionId: {
+          contains: `${trainingSession}`,
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
 
     // create user in database
     try {
@@ -40,22 +49,37 @@ export default async function handle(
           ...user,
         },
       });
-      if (!emailVerificationDisabled)
-        await sendVerificationEmail(userData, callbackUrl);
       capturePosthogEvent(user.email, "user created");
 
-      updateCandidateSourcingOnAirtable(user.email, trainingSession);
-      res.json(userData);
+      res.json({
+        message: "Compte créé avec succès",
+        formId: form.id,
+        id: userData.id,
+        email: user.email,
+      });
+
 
     } catch (e) {
       if (e.code === "P2002") {
+
+        let foundUser = await prisma.user.findUnique({
+          where: {
+            email: `${email}`
+          }, select: {
+            id: true,
+            email: true
+          }
+        })
+
         return res.status(409).json({
-          error: `un utilisateur avec ${
-            e.meta.target[0] === "email"
-              ? "cette adresse e-mail"
-              : "ce numéro de téléphone"
-          } existe déjà`,
+          error: `un utilisateur avec ${e.meta.target[0] === "email"
+            ? "cette adresse e-mail"
+            : "ce numéro de téléphone"
+            } existe déjà`,
           errorCode: e.code,
+          formId: form.id,
+          id: foundUser.id,
+          email: foundUser.email,
         });
       } else {
         return res.status(500).json({
@@ -68,11 +92,11 @@ export default async function handle(
   // GET /api/public/users
   else if (req.method === "GET") {
     // Check Authentication and user role
-    const session = await getSession({req: req});
-    if (!session) return res.status(401).json({message: "Not authenticated"});
+    const session = await getSession({ req: req });
+    if (!session) return res.status(401).json({ message: "Not authenticated" });
 
     if (session.user.role === UserRole.PUBLIC)
-      return res.status(403).json({message: "Forbidden"});
+      return res.status(403).json({ message: "Forbidden" });
     const usersData = await prisma.user.findMany({
       select: {
         id: true,
